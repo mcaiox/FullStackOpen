139dbff3025f5721de00dafcc7e056e073967c06
Chin <mcaiox@icloud.com>
2021-12-12 18:42:28 +0000

    Exercise 3.1

--- part3/web and express/index.js
+++ part3/web and express/index.js
@@ -49,11 +49,28 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
-  notes.push(note);
 });
 
 const PORT = 3001;
@@ -61,4 +78,4 @@ app.listen(PORT, () => {
   console.log(`Server is running  on port ${PORT}`);
 });
 
-// Ended lesson just before the start of Important sidenote, - Postman also allows users to save requests, but the situation can get quite chaotic especially when you're working on multiple unrelated projects.
+// Ended lesson
