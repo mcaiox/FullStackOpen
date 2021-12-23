ce15f3d4f1817bd472520017391178a66ca24a75
Chin <mcaiox@icloud.com>
2021-12-23 20:30:27 +0000

    Re-organised repository

--- part8-t/library-backend/utils/config.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-const { MONGODB_URI, JWT_SECRET } = process.env;
-
-module.exports = { MONGODB_URI, JWT_SECRET };
--- part9-t/typed-courseinfo/src/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Helper function for exhaustive type checking
- */
-export const assertNever = (value: never): never => {
-  throw new Error(
-    `Unhandled discriminated union member: ${JSON.stringify(value)}`
-  );
-};
