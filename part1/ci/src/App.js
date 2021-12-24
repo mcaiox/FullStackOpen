b0ca3ae3983ceb28e1af222cd353b3bfe8e99e83
Chin <mcaiox@icloud.com>
2021-12-24 18:54:53 +0000

    excersise 1.1 completed

--- part1/ci/src/App.js
+++ part1/ci/src/App.js
@@ -11,15 +11,15 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>{props.part + props.exercise}</p>
+      <p>{props.part + " " + props.exercise}</p>
     </>
   );
 };
 
-const Total = (Props) => {
+const Total = (props) => {
   return (
     <>
-      <p>Header</p>
+      <p>{"Total: " + props.total}</p>
     </>
   );
 };
@@ -37,15 +37,17 @@ const App = () => {
     <div>
       <Header course={course} />
       <p>
-        {part1} {exercises1}
+        <Content part={part1} exercise={exercises1} />
       </p>
       <p>
-        {part2} {exercises2}
+        <Content part={part2} exercise={exercises2} />
       </p>
       <p>
-        {part3} {exercises3}
+        <Content part={part3} exercise={exercises3} />
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>
+        <Total total={exercises1 + exercises2 + exercises3} />
+      </p>
     </div>
   );
 };
