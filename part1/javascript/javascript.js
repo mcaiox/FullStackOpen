5fce13b5c2f27bd3b09c85dacab3cdd9984e9dd1
Chin <mcaiox@icloud.com>
2021-12-27 19:11:16 +0000

    js work-along

--- part1/javascript/javascript.js
+++ part1/javascript/javascript.js
@@ -5,3 +5,31 @@ const [first, second, ...rest] = t;
 console.log(rest);
 
 //Complete Arrays section, at Objects bit now
+
+const object1 = {
+  name: "Arto Hellas",
+  age: 35,
+  education: "PHD",
+};
+
+const object2 = {
+  name: "Full Stack web application development",
+  level: "Intermediate studies",
+  size: 5,
+};
+
+const object3 = {
+  name: {
+    first: "Dan",
+    last: "Abramov",
+  },
+  grades: [
+    { age: 2, level: 3 },
+    { age: 5, level: 3 },
+  ],
+  department: "Stanford University",
+};
+
+object3.address = "Helsinki";
+object3["secret number"] = 12341;
+console.log(object3);
