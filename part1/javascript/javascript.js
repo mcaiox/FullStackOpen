52d7118516926b7287ef805f93d01b3dcd6fe9e1
Chin <mcaiox@icloud.com>
2021-12-28 21:04:14 +0000

    Part 1 redo - functions complete

--- part1/javascript/javascript.js
+++ part1/javascript/javascript.js
@@ -2,9 +2,7 @@ const t = [1, 2, 3, 4, 5];
 
 const [first, second, ...rest] = t;
 
-console.log(rest);
-
-//Complete Arrays section, at Objects bit now
+// console.log(rest);
 
 const object1 = {
   name: "Arto Hellas",
@@ -32,4 +30,33 @@ const object3 = {
 
 object3.address = "Helsinki";
 object3["secret number"] = 12341;
-console.log(object3);
+// console.log(object3);
+
+// You gotta use parameters when the function has more than one parameter.
+const sum = (p1, p2) => {
+  console.log(p1);
+  console.log(p2);
+  return p1 + p2;
+};
+
+const result = sum(1, 5);
+console.log(result);
+
+//you can use no parameters if the function has one parameter and position it RHS of = and LHS of =>
+
+const squareWithBody = (p) => {
+  console.log(p);
+  return p * p;
+};
+
+//you can use no curly brace if the body is a singular expression
+
+const square = (p) => p * p;
+
+console.log(square(2));
+
+const twotime = (p1, p2) => p1 * p2;
+
+console.log("twotime: " + twotime(2, 3));
+
+//Functions completed, onto excercises next lesson
