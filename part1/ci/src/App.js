5af0c19e9f1c8c0f014993c6ad6f7dd4b76e027f
Chin <mcaiox@icloud.com>
2021-12-29 20:36:04 +0000

    excercise 1.3

--- part1/ci/src/App.js
+++ part1/ci/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = (props) => {
+  console.log(props);
   return (
     <>
       <h1>{props.course}</h1>
@@ -26,28 +27,36 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   const Content = (props) => {
+    console.log(props);
     return (
       <>
-        <Part part={part1} exercise={exercises1} />
-        <Part part={part2} exercise={exercises2} />
-        <Part part={part3} exercise={exercises3} />
+        <Part part={part1.name} exercise={part1.exercises} />
+        <Part part={part2.name} exercise={part2.exercises} />
+        <Part part={part3.name} exercise={part3.exercises} />
       </>
     );
   };
+
   return (
     <div>
       <Header course={course} />
       <Content />
       <p>
-        <Total total={exercises1 + exercises2 + exercises3} />
+        <Total total={part1.exercises + part2.exercises + part3.exercises} />
       </p>
     </div>
   );
