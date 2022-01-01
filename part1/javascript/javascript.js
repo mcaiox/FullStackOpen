fe61f3196d27babb7acfbc573aa35d4922c77ff7
Chin <mcaiox@icloud.com>
2022-01-01 21:29:41 +0000

    Objects section completed

--- part1/javascript/javascript.js
+++ part1/javascript/javascript.js
@@ -4,10 +4,16 @@ const [first, second, ...rest] = t;
 
 // console.log(rest);
 
-const object1 = {
+const arto = {
   name: "Arto Hellas",
   age: 35,
   education: "PHD",
+  greet: function () {
+    console.log("Hello my name is " + this.name);
+  },
+  doAddition: function (a, b) {
+    console.log(a + b);
+  },
 };
 
 const object2 = {
@@ -60,3 +66,20 @@ const twotime = (p1, p2) => p1 * p2;
 console.log("twotime: " + twotime(2, 3));
 
 //Functions completed, onto excercises next lesson
+
+setTimeout(arto.greet.bind(arto), 1000);
+
+const referenceToGreet = arto.greet.bind(arto);
+referenceToGreet();
+arto.growOlder = function () {
+  this.age += 1;
+};
+
+console.log(arto.age); // 35 is printed
+arto.growOlder();
+console.log(arto.age); // 36 is printed
+
+arto.doAddition(1, 4);
+const referenceToAddition = arto.doAddition;
+
+referenceToAddition(10, 15);
