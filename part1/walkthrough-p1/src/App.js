f29a8b06e4e863c2dbae55316a1b8e0f2812b737
Chin <mcaiox@icloud.com>
2022-01-12 01:30:48 +0000

    Handling arrays and conditional rendering  section completed

--- part1/walkthrough-p1/src/App.js
+++ part1/walkthrough-p1/src/App.js
@@ -1,24 +1,66 @@
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [clicks, setClicks] = useState({
+//     left: 0,
+//     right: 0,
+//   });
+//   const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+//   const handleRightClick = () =>
+//     setClicks({ ...clicks, right: clicks.right + 1 });
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {clicks.right}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//Completed complex state, need to start on handling arrays next in section d
+
 import React, { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
-
 export default App;
 
-//Completed complex state, need to start on handling arrays next in section d
+//Completed Conditional rendering, ready to start Old React section
