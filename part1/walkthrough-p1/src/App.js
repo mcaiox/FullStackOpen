856352c5ae55e73bbb985968ca2868e5fe379775
Chin <mcaiox@icloud.com>
2022-02-04 21:32:14 +0000

    completed functions that return functions

--- part1/walkthrough-p1/src/App.js
+++ part1/walkthrough-p1/src/App.js
@@ -3,63 +3,20 @@ import React, { useState } from "react";
 const App = () => {
   const [value, setValue] = useState(10);
 
-  const hello = (who) => {
-    const handler = () => {
-      console.log("hello", who);
-    };
-    return handler;
+  const setToValue = (newValue) => () => {
+    setValue(newValue);
   };
 
   return (
     <div>
       {value}
-      <button onClick={hello("world")}>button</button>
-      <button onClick={hello("react")}>button</button>
-      <button onClick={hello("function")}>button</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
 
 export default App;
 
-//started Function that returns a function @ Now the application has three buttons with event handlers defined by the hello function that accepts a parameter. The first button is defined as
-
-// import React, { useState } from "react";
-
-// const History = (props) => {
-//   if (props.allClicks.length === 0) {
-//     return <div>the app is used by pressing buttons</div>;
-//   }
-//   return <div>button press history: {props.allClicks.join(" ")}</div>;
-// };
-
-// const Button = ({ handleClick, text }) => (
-//   <button onClick={handleClick}>{text}</button>
-// );
-
-// const App = () => {
-//   const [left, setLeft] = useState(0);
-//   const [right, setRight] = useState(0);
-//   const [allClicks, setAll] = useState([]);
-
-//   const handleLeftClick = () => {
-//     setAll(allClicks.concat("L"));
-//     setLeft(left + 1);
-//   };
-
-//   const handleRightClick = () => {
-//     setAll(allClicks.concat("R"));
-//     setRight(right + 1);
-//   };
-
-//   return (
-//     <div>
-//       {left}
-//       <Button handleClick={handleLeftClick} text="left" />
-//       <Button handleClick={handleRightClick} text="right" />
-//       {right}
-//       <History allClicks={allClicks} />
-//     </div>
-//   );
-// };
-// export default App;
+// completed fn => fn and started Passing Event Handlers to Child Components
