b94e60377dcf819c9f67bbdb01a1c199a6ffa5a9
Chin <mcaiox@icloud.com>
2022-02-06 00:19:39 +0000

    completed section d, started Excercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const Display = (props) => (
+    <div>
+      {props.text} {props.value}
+    </div>
+  );
+  const DisplayNeutral = (props) => <div>Neutral {props.value}</div>;
+  const DisplayBad = (props) => <div>Bad {props.value}</div>;
+
+  const setTo = (type) => () => {
+    if (type === "good") {
+      setGood(good + 1);
+    } else if (type === "neutral") {
+      setNeutral(neutral + 1);
+    } else if (type === "bad") {
+      setBad(bad + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <button onClick={setTo("good")}>Good</button>
+      <button onClick={setTo("neutral")}>Neutral</button>
+      <button onClick={setTo("bad")}>Bad</button>
+
+      <h2>Statistics</h2>
+      <Display value={good} text="Good" />
+      <Display value={neutral} text="Neutral" />
+      <Display value={bad} text="Bad" />
+    </div>
+  );
+};
+
+export default App;
+
+// Currently doing 1.6 unicafe step
+// Need to make custom components for buttons and display - also add functionality that makes they polymorphic
+//Look at previouse excerises and refactor code
