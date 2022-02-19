94c03dea2c43f3ea1fe804d4c3a9f96d70521f8a
Muhammad Ahmed <mcaiox@icloud.com>
2022-02-19 01:08:54 +0000

    Completed excercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,32 +4,43 @@ const Button = (props) => (
 );
 const Heading = (props) => <h1>{props.text}</h1>;
 
-const Display = (props) => (
-  <div>
-    {props.text} {props.value}
-  </div>
-);
+const Display = (props) =>
+  props.text === "Positive" ? (
+    <div>
+      {props.text} {props.value} %
+    </div>
+  ) : (
+    <div>
+      {props.text} {props.value}
+    </div>
+  );
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
   const setTo = (type) => () => {
     switch (type) {
       case "good":
         setGood(good + 1);
+        setAll(all + 1);
         break;
       case "neutral":
         setNeutral(neutral + 1);
+        setAll(all + 1);
         break;
       case "bad":
         setBad(bad + 1);
+        setAll(all + 1);
         break;
       default:
         console.error("Unknown type for feedback button");
     }
   };
-
+  const avg = all > 0 ? ((good + (bad > 0 ? bad * -1 : 0)) / all) * 100 : 0;
+  const positive = good > 0 ? (good / all) * 100 : 0;
   return (
     <div>
       <Heading text="Give feedback" />
@@ -41,6 +52,9 @@ const App = () => {
       <Display value={good} text="Good" />
       <Display value={neutral} text="Neutral" />
       <Display value={bad} text="Bad" />
+      <Display value={all} text="All" />
+      <Display value={avg} text="Average" />
+      <Display value={positive} text="Positive" />
     </div>
   );
 };
