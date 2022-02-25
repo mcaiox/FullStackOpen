58e4901f22e2bb2f2df91b4516b38cdff5ae6a49
Muhammad Ahmed <mcaiox@icloud.com>
2022-02-25 06:28:49 +0000

    Completed Exercise 2.1 and removed irrelevant files

---
+++ part2/course-contents/src/components/Content.js
@@ -0,0 +1,11 @@
+import Part from "./Part";
+
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </>
+);
+
+export default Content;
--- part2/course-contents/src/components/Course.js
+++ part2/course-contents/src/components/Course.js
@@ -1,48 +1,12 @@
-import React from "react";
+import Header from "./Header";
+import Content from "./Content";
 
-const Header = (props) => {
-  return <h1>{props.name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  const partsList = parts.map((item) => {
-    return <Part key={item.id} name={item.name} exercises={item.exercises} />;
-  });
-
-  const noParts = !Array.isArray(partsList) || !partsList.length;
-
-  return (
-    <div>
-      {noParts && <p>This course doesn't have any parts yet.</p>}
-      {!noParts && partsList}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, item) => acc + item.exercises, 0);
-
-  return (
-    <p>
-      <strong>Total of {total} exercises</strong>
-    </p>
-  );
-};
 
 const Course = ({ course }) => {
   return (
     <div>
-      <Header name={course.name} />
+      <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
     </div>
   );
 };
