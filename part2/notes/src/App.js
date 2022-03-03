48816e7b6cb0f9a87d7a4a95585a0c5ad0ec3678
Muhammad Ahmed <mcaiox@icloud.com>
2022-03-03 07:35:42 +0000

    Complete section on Forms up until excercises

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,21 +4,39 @@ import Note from "./components/Note";
 const App = (props) => {
   const [Notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
-    console.log("Button clicked", event.type);
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: Notes.length + 1,
+    };
+
+    setNotes(Notes.concat(noteObject));
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
+
+  const notesToShow = showAll
+    ? Notes
+    : Notes.filter((note) => note.important === true);
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {Notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -6,7 +6,7 @@ const notes = [
     id: 1,
     content: "HTML is easy",
     date: "2019-05-30T17:30:31.098Z",
-    important: true,
+    important: false,
   },
   {
     id: 2,
