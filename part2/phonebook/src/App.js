b891027a6f08e09edbc5a6fbe805afcc98c7e2a1
Muhammad Ahmed <mcaiox@icloud.com>
2022-06-09 13:54:24 +0100

    add functionality for form input and state

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,44 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
   const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons[persons.length - 1].id + 1,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onSubmit={() => {}}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <>
+        {persons.map((person) => (
+          <li key={person.id}>
+            {person.name} {person.id}
+          </li>
+        ))}
+      </>
       <div>debug: {newName}</div>
     </div>
   );
