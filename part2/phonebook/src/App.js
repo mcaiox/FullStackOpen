590ce0c2afc7bf7091d291cf5a4004933c86782a
Muhammad Ahmed <mcaiox@icloud.com>
2022-09-25 02:34:10 +0100

    some work on filter functionality for excercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [showAll, setShowAll] = useState(false);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,7 +20,7 @@ const App = () => {
     } else {
       console.log("creating new person object");
       const personObject = {
-        name: newName,
+        name: newName.toLowerCase(),
         number: newNumber,
       };
       setPersons(persons.concat(personObject));
@@ -32,6 +36,10 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter((person) => person.name.includes("ad"));
   return (
     <div>
       <h2>Phonebook</h2>
@@ -48,7 +56,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
