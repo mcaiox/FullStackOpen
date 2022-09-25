80901134a9ac6c0f0eba5a1879940c8cd5ea525c
Muhammad Chin Ahmed <mcaiox@icloud.com>
2022-09-25 22:56:49 +0100

    add filter functionality for exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,20 +2,25 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    { name: "arto hellas", number: "040-123456", id: 1 },
+    { name: "ada lovelace", number: "39-44-5323523", id: 2 },
+    { name: "dan abramov", number: "12-43-234345", id: 3 },
+    { name: "mary poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [showAll, setShowAll] = useState(false);
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     console.log(persons.some((person) => person.name === newName));
 
-    if (persons.some((person) => person.name === newName)) {
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
       alert(`${newName} is already added to phonebook`);
     } else {
       console.log("creating new person object");
@@ -36,13 +41,23 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  };
 
   const personsToShow = showAll
     ? persons
-    : persons.filter((person) => person.name.includes("ad"));
+    : persons.filter((person) => person.name.includes(newFilter.toLowerCase()));
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with
+          <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
