7374aebdfdf8c48ca5b7b09eaac4d180f0703b01
Muhammad Ahmed <mcaiox@icloud.com>
2022-10-02 14:51:47 +0100

    Resolved dependency issues

--- part2/p2-walkthrough/rendering_collections/src/App.js
+++ part2/p2-walkthrough/rendering_collections/src/App.js
@@ -4,12 +4,13 @@ import Note from "./components/Note";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+  // const [newNote, setNewNote] = useState("");
+  // const [showAll, setShowAll] = useState(true);
   useEffect(() => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
       console.log("promise fulfilled");
+      console.log(response.data);
       setNotes(response.data);
     });
   }, []);
