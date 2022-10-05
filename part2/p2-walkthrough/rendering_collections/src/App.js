053f2baecc96ff05525afc6673494fc3672fa13a
Muhammad Ahmed <mcaiox@icloud.com>
2022-10-05 22:56:07 +0100

    Completed section on effect hooks and the github repository for notes being stored in state

--- part2/p2-walkthrough/rendering_collections/src/App.js
+++ part2/p2-walkthrough/rendering_collections/src/App.js
@@ -6,13 +6,18 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
-  useEffect(() => {
+  const hook = () => {
     console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
+    axios
+    .get("http://localhost:3001/notes")
+    .then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
-  }, []);
+  };
+
+  useEffect(hook, []);
+
   console.log("render", notes.length, "notes");
   return (
     <div>
