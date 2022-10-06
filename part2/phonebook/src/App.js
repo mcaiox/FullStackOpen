0399ab008260e0340d7f75aadb8f012e94043482
Muhammad Ahmed <mcaiox@icloud.com>
2022-10-06 21:36:12 +0100

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./persons";
 import PersonForm from "./personForm";
 import Filter from "./filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "arto hellas", number: "040-123456", id: 1 },
-    { name: "ada lovelace", number: "39-44-5323523", id: 2 },
-    { name: "dan abramov", number: "12-43-234345", id: 3 },
-    { name: "mary poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
   const [view, setView] = useState(true);
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const addPerson = (event) => {
     event.preventDefault();
